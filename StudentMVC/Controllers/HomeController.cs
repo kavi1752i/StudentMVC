9791234d79ebc76c6effb353761caa3614e99534
kaviyarasi <kaviyarasi.nallathambi@gmail.com>
2025-12-10 14:53:22 +0530

    test username

--- StudentMVC/Controllers/HomeController.cs
+++ StudentMVC/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace StudentMVC.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
